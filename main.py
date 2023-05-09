b5d4185107b4e25cc15326483ae90035601ba6c3
Andrei Matei <110837739+MateiAnd@users.noreply.github.com>
2023-05-09 12:00:43 +0300

    adaugat nume la support gpt

--- main.py
+++ main.py
@@ -684,6 +684,8 @@ async def on_message(message: discord.Message):
             except:
                 async with aiofiles.open('./chat_gpt/init_chat.json', 'r') as f:
                     message_history = json.loads(await f.read())
+                    format_content = message_history["msg"][0]["content"].format(nume=message.author.mention)
+                    message_history["msg"][0]["content"] = format_content
 
             message_history["msg"].append({'role': 'user', 'content': message.content})
 
